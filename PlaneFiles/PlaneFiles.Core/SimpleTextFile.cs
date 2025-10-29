79c282f399b78b6ac11bebb29a10e79e944e05e3
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2025-10-28 19:50:03 -0500

    Updates Plane Files

---
+++ PlaneFiles/PlaneFiles.Core/SimpleTextFile.cs
@@ -0,0 +1,22 @@
+﻿namespace PlaneFiles.Core
+{
+    public class SimpleTextFile
+    {
+        private readonly string _path;
+
+        public SimpleTextFile(string path)
+        {
+            _path = path;
+        }
+
+        public void writeAllLines(string[] lines)
+        {
+            File.WriteAllLines(_path, lines);            
+        }
+
+        public string[] ReadAllLines()
+        {
+            return File.ReadAllLines(_path);
+        }
+    }
+}
---
+++ PlaneFiles/PlaneFiles.UI.Console/Program.cs
@@ -0,0 +1,9 @@
+﻿using PlaneFiles.Core;
+
+var textFile = new SimpleTextFile("data.txt");
+var linesReaded = textFile.ReadAllLines();
+
+foreach (var line in linesReaded)
+{
+    Console.WriteLine(line);
+}
